80569b13c916995e0f0c4ff315e9838799f73c71
Jackson Taylor <closprieur@gmail.com>
2016-08-27 19:28:33 +1000

    Create QuizModel and Question classes

---
+++ QuizModel.swift
@@ -0,0 +1,13 @@
+//
+//  QuizModel.swift
+//  Quiz App
+//
+//  Created by Jackson Taylor on 27/08/2016.
+//  Copyright © 2016 Jackson Taylor. All rights reserved.
+//
+
+import UIKit
+
+class QuizModel: NSObject {
+
+}
