3e02e4d55687e452029837890c80f865eabb52cd
Jackson Taylor <closprieur@gmail.com>
2016-08-28 17:00:07 +1000

    Add JSON parsing for QuestionData in QuizModel

--- QuizModel.swift
+++ QuizModel.swift
@@ -12,16 +12,50 @@ class QuizModel: NSObject {
     
     func getQuestions() -> [Question] {
     
-        // Questions to return
+        // Array of question objects
         var questions: [Question] = [Question]()
         
-        // TODO: Get JSON file
+        // Get JSON array of dictionaries
         
-        // TODO: Parse JSON file
+        // Loop through each dictionary and assign values to our question objects
         
-        // TODO: Return list of question objects
+        // Return list of question objects
         return questions
     
+    }
+    
+    func getLocalJsonFile() -> [NSDictionary] {
+    
+        // Get an NSURL object pointing to the json file in our app bundle
+        let appBundlePath: String? = NSBundle.mainBundle().pathForResource("QuestionData", ofType: "json")
+    
+        // Use optional binding to check if path exists
+        if let actualBundlePath = appBundlePath {
+        
+            // Path exists. Create an NSURL
+            let urlPath: NSURL = NSURL(fileURLWithPath: actualBundlePath)
+            
+            // Creata NSData object for json
+            let jsonData: NSData? = NSData(contentsOfURL: urlPath)
+            
+            if let actualJsonData = jsonData {
+            
+                // NSData exists; use the NSJSON serialization classes to parse the data and create the array of dictionaries
+                // let arrayOfDictionaries: [NSDictionary] =
+            
+            
+            } else {
+            
+                // Handle error for NSData does not exist
+            
+            }
+        
+        } else {
+        
+            // Path to json file in the bundle does not exist
+        
+        }
+        
     }
 
 }
